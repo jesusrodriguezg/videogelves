1c6a822e9fe770128d40e065f74e4edeb0f01ada
Jesús Rodríguez <sanedu@gmail.com>
2021-05-28 06:42:03 +0200

    Diseño inicial
    Primera versión del diseño en la parte frontend.

---
+++ client/src/app/loggedout/loggedout.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-loggedout',
+  templateUrl: './loggedout.component.html',
+  styleUrls: ['./loggedout.component.css']
+})
+export class LoggedoutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
